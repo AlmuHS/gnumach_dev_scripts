bab3e01797de7df9ee3fa6c732e726b97d0a86a5
Almudena Garcia <liberamenso10000@gmail.com>
2023-02-22 23:44:32 +0100

    add script to create vm

---
+++ create_vm.sh
@@ -0,0 +1,25 @@
+OPTIONS="$1"
+VM_OPTIONS=""
+
+mkdir $HOME/hurd_qemu
+cd $HOME/Downloads
+
+if [[ $OPTIONS == "--from-installer" ]];
+then
+	DVD_DOWNLOAD="https://cdimage.debian.org/cdimage/ports/latest/hurd-i386/current/debian-sid-hurd-i386-DVD-1.iso"
+	wget $DVD_DOWNLOAD
+	
+	qemu-img create -f raw $HOME/hurd_qemu/hurd.img 10G
+	
+	VM_OPTIONS="-i"
+else
+	if [[ ! -f debian-hurd.img.gz && ! -f debian-hurd.img ]]
+	then
+		IMG_DOWNLOAD="https://cdimage.debian.org/cdimage/ports/latest/hurd-i386/debian-hurd.img.gz"
+		wget $IMG_DOWNLOAD
+		gzip -vd debian-hurd.img.gz
+		mv debian-hurd.img $HOME/hurd_qemu/hurd.img
+	fi
+fi
+
+./qemu-hurd.sh $VM_OPTIONS
