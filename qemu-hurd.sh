c1d577aa6470ba5f74adec5a83a38b6e108a72b5
Almudena Garcia <liberamenso10000@gmail.com>
2023-02-22 23:38:34 +0100

    qemu-hurd.sh: add multiple boot modes to script

--- qemu-hurd.sh
+++ qemu-hurd.sh
@@ -1,5 +1,5 @@
+#+BEGIN_SRC sh :results output :exports both
 #!/bin/bash
-
 #modprobe kvm
 #modprobe kvm_intel
 #qemu-img create -f qcow2 hurd.img 20G
@@ -9,12 +9,20 @@ CDROM="$HOME/Descargas/debian-sid-hurd-i386-DVD-1.iso"
 # If I ever need to add a cdrom
     echo "running ssh"
 
+BOOT='c'
+
+if [[ $1 == "-i" || $1 == "--install" ]];
+then
+	echo "installer mode"
+	BOOT='d'
+fi
+
 OPTIONS="-device ahci,id=ahci1 												\
                      -drive id=root,if=none,media=disk,format=raw,file=$FILE \
 										 -device ide-hd,drive=root,bus=ahci1.1				 \
 										 -drive id=cd,file=$CDROM,media=cdrom					\
                      -smp 8                                       \
-                     -boot d                                      \
+                     -boot $BOOT                                      \
                      -net user,hostfwd=tcp:127.0.0.1:2222-:22     \
                      -net nic,model=e1000                      \
                      -no-reboot                                   \
@@ -22,13 +30,17 @@ OPTIONS="-device ahci,id=ahci1 												\
                      -vga std                                     \
                      -display gtk"				
 
-if [[ "$1" = "--runtime" ]];
+if [[ $# -eq 0 ||  $1 == "-i" || $1 == "--install" ]];
 then
 	echo "kvm enabled"
 	OPTIONS="$OPTIONS -enable-kvm"
+elif [[ $1 == '-D' || $1 == '--debug' ]];
+then
+	echo "debug mode"
+	OPTIONS="-S $OPTIONS"
 fi
 
-qemu-system-i386 -S -s -m $MEMORY "$OPTIONS" 
+qemu-system-i386 -s -m $MEMORY $OPTIONS 
                      # -machine kernel_irqchip=off                  \
                      #					\
                      #-drive id=root,if=none,format=raw,media=disk,file=/dev/sdb \
