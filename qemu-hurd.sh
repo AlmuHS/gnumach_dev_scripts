b2d8f8371dad7d09409c0221ac81200895acaee8
Almudena Garcia <liberamenso10000@gmail.com>
2023-03-10 22:51:00 +0100

    qemu-hurd.sh: improve network connection

--- qemu-hurd.sh
+++ qemu-hurd.sh
@@ -23,7 +23,7 @@ OPTIONS="-device ahci,id=ahci1 												\
                      -drive id=root,if=none,media=disk,format=raw,file=$FILE \
 										 -device ide-hd,drive=root,bus=ahci1.1				 \
 										 -drive id=cd,if=none,file=$CDROM,media=cdrom					\
-										 -device ide-cd,drive=cd,bus=ahci1.2	\
+										 -device ide-cd,drive=cd\
                      -smp 8                                       \
                      -boot $BOOT                                      \
                      -net user,hostfwd=tcp:127.0.0.1:2222-:22     \
