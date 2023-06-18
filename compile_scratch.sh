f55927cbdb6bde979d582b5906126f16355fabc6
Almudena Garcia <liberamenso10000@gmail.com>
2023-06-18 02:40:12 +0200

    removed redundant --disable-ide flag

--- compile_scratch.sh
+++ compile_scratch.sh
@@ -14,5 +14,5 @@ rm -rf build
 mkdir build
 cd build
 echo "compiling for $NUM_CPUS cpus"
-../configure --host=i686-gnu CC='gcc -m32' LD='ld -melf_i386' --enable-apic --enable-kdb --enable-ncpus=$NUM_CPUS --disable-ide --disable-linux-groups
+../configure --host=i686-gnu CC='gcc -m32' LD='ld -melf_i386' --enable-apic --enable-kdb --enable-ncpus=$NUM_CPUS --disable-linux-groups
 make gnumach.gz
